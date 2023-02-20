3126157a9305e1733d2a7c0b73942e6c23c041b9
hsuuuk <hsuuuk123@gmail.com>
2023-02-20 23:01:03 +0900

    add policy

---
+++ BikeDangerAreaAlert/Setting/PolicyController.swift
@@ -0,0 +1,40 @@
+//
+//  PolicyController.swift
+//  BikeDangerAreaAlert
+//
+//  Created by 심현석 on 2023/02/20.
+//
+
+import UIKit
+import SnapKit
+import WebKit
+
+class PolicyController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupWebView()
+    }
+    
+    func setupWebView() {
+        let webView = WKWebView()
+        view.addSubview(webView)
+        webView.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalTo(view.safeAreaLayoutGuide)
+        }
+        webView.navigationDelegate = self
+        
+        if let htmlPath = Bundle.main.path(forResource: "개인정보처리방침", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        } else {
+            print("fail")
+        }
+    }
+}
+
+
